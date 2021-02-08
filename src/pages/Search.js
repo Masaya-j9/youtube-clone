962831aa486544fc25d884edbe627f81ecddbaf0
Masaya-j9 <masaya.spectre@gmail.com>
2021-02-09 02:24:58 +0900

    first push

---
+++ src/pages/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+export const Search = () => {
+  return (
+    <Layout>
+      search
+    </Layout>
+  )
+}
+
+export default Search
---
+++ src/pages/Watch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export const Watch = () => {
+  return (
+    <Layout>
+      Watch
+    </Layout>
+  )
+}
+
+export default Watch
